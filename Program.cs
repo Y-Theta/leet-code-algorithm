0369e7327d257440bccefcc6b1d52ac449d8c8cf
杨杰铭 <yangjm@mctech.vip>
2025-07-13 06:17:20 +0800

    init project with problem 135

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+var count = new Solution().Candy(new int[] { 1,3,2,2,1 });
+var count1 = new Solution().Candy(new int[] { 1,2,3,1,0 });
+Debug.WriteLine($"{count} {count1}");
